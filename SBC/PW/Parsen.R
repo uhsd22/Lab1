99345fd29cb8bb035cbfbf5d03853ea8696ebfb5
uhsd22 <71466568+uhsd22@users.noreply.github.com>
2020-12-22 12:28:20 +0300

    Update Parsen.R

--- SBC/PW/Parsen.R
+++ SBC/PW/Parsen.R
@@ -1,21 +1,21 @@
 
 colors <- c("setosa" = "darkorchid3", "versicolor" = "firebrick1", "virginica"= "aquamarine3", "unknown"="yellow")
 
-#Ôóíêöèÿ îòðèñîâêè íàáîðà
-plotIris <- function(ir=iris,label="Êëàññèôèêàöèÿ"){
+#Ã”Ã³Ã­ÃªÃ¶Ã¨Ã¿ Ã®Ã²Ã°Ã¨Ã±Ã®Ã¢ÃªÃ¨ Ã­Ã Ã¡Ã®Ã°Ã 
+plotIris <- function(ir=iris,label="ÃŠÃ«Ã Ã±Ã±Ã¨Ã´Ã¨ÃªÃ Ã¶Ã¨Ã¿"){
   plot(ir[, 3:4], pch = 21, bg = colors[ir$Species], col = colors[ir$Species],
        xlab="Length of petal",ylab="Width of petal",main=label,asp = 1)
 }
 
 plotIris()
-#Ôóíêöèÿ ìåòðèêè
+#Ã”Ã³Ã­ÃªÃ¶Ã¨Ã¿ Ã¬Ã¥Ã²Ã°Ã¨ÃªÃ¨
 EucDist <- function(u, v) {
   sqrt(sum((u - v)^2))
 }
 
 
-#Ôóíêöèÿ îòðèñîâêè êàðòû êëàññèôèêàöèè àëãîðèòìà parsen
-classMapParsen <- function(ir=iris,h=2,K=kE){
+#Ã”Ã³Ã­ÃªÃ¶Ã¨Ã¿ Ã®Ã²Ã°Ã¨Ã±Ã®Ã¢ÃªÃ¨ ÃªÃ Ã°Ã²Ã» ÃªÃ«Ã Ã±Ã±Ã¨Ã´Ã¨ÃªÃ Ã¶Ã¨Ã¨ Ã Ã«Ã£Ã®Ã°Ã¨Ã²Ã¬Ã  parsen
+classMapParsen <- function(ir=iris,h,K=kE){
   for (i in seq(0,7,0.1)) {
     for (j in seq(0,2.5,0.1)){
       plotParsen(i,j,ir,h,K)
@@ -23,25 +23,25 @@ classMapParsen <- function(ir=iris,h=2,K=kE){
   }
 }
 
-# Ôóíêöèè ÿäåð
-kR <- function(z) 0.5 * (abs(z)<=1) # Ïðÿìîóãîëüíîå
-kT <- function(z) (1 - abs(z))*(abs(z)<=1) #  Òðåóãîëüíîå
-kQ <- function(z) (15/16)*(1 - z^2)^2 * (abs(z)<=1) # Êâàðòè÷åñêîå
-kE <- function(z) (3/4)*(1-z^2) * (abs(z)<=1) # Åïàíå÷íèêîâà
+# Ã”Ã³Ã­ÃªÃ¶Ã¨Ã¨ Ã¿Ã¤Ã¥Ã°
+kR <- function(z) 0.5 * (abs(z)<=1) # ÃÃ°Ã¿Ã¬Ã®Ã³Ã£Ã®Ã«Ã¼Ã­Ã®Ã¥
+kT <- function(z) (1 - abs(z))*(abs(z)<=1) #  Ã’Ã°Ã¥Ã³Ã£Ã®Ã«Ã¼Ã­Ã®Ã¥
+kQ <- function(z) (15/16)*(1 - z^2)^2 * (abs(z)<=1) # ÃŠÃ¢Ã Ã°Ã²Ã¨Ã·Ã¥Ã±ÃªÃ®Ã¥
+kE <- function(z) (3/4)*(1-z^2) * (abs(z)<=1) # Ã…Ã¯Ã Ã­Ã¥Ã·Ã­Ã¨ÃªÃ®Ã¢Ã 
 kG <- function(z) {
-  return ((2*pi)^(-0.5)*exp(-0.5*(z^2))) #Ãàóññ
+  return ((2*pi)^(-0.5)*exp(-0.5*(z^2))) #ÃƒÃ Ã³Ã±Ã±
 }
 
-#Ïàðçåíîâñêîå îêíî #h_opt: kR - 0.35; kT - 0.35, kQ - 0.35; kE - 0.35; kG - 0.1
-parsen <- function(x, z, h, K){
-  m <- dim(x)[1]
-  n <- dim(x)[2]-1
-  count_classes <- length(names(table(x[,n+1])))
+#ÃÃ Ã°Ã§Ã¥Ã­Ã®Ã¢Ã±ÃªÃ®Ã¥ Ã®ÃªÃ­Ã® #h_opt: kR - 0.35; kT - 0.35, kQ - 0.35; kE - 0.35; kG - 0.1
+parsen <- function(xx, z, h, K){
+  row <- dim(xx)[1]
+  col <- dim(xx)[2]-1
+  count_classes <- length(names(table(xx[,col+1])))
   classes <- rep(0,count_classes)
-  names(classes) <- names(table(x[,n+1]))
-  for(i in 1:m){
-    y <- x[i,n+1]
-    dist <- EucDist(x[i,1:n],z)
+  names(classes) <- names(table(xx[,col+1]))
+  for(i in 1:row){
+    y <- xx[i,col+1]
+    dist <- EucDist(xx[i,1:col],z)
     w <- K(dist/h)
     classes[y] <- classes[y] + w
   }
@@ -49,17 +49,17 @@ parsen <- function(x, z, h, K){
   else class <- "unknown"
   return(class)
 }
-LOO_parsen <- function(x,K,labels="LOO äëÿ ïàðçåíîâñêîãî îêíà"){
-  m <- dim(x)[1]
-  n <- dim(x)[2] - 1
+LOO_parsen <- function(x,K,labels="LOO Ã¤Ã«Ã¿ Ã¯Ã Ã°Ã§Ã¥Ã­Ã®Ã¢Ã±ÃªÃ®Ã£Ã® Ã®ÃªÃ­Ã "){
+  row <- dim(xx)[1]
+  col <- dim(xx)[2] - 1
   params <- seq(0.1,2,0.05)
   mark <- rep(0,length(params))
   for (h in 1:length(params)){
-    for (i in 1:m){
-      x1 <- x[-i,]
-      class1 <- parsen(x1,x[i,1:n],params[h],K)
-      class2 <- x[i,n+1]
-      if (class1 != class2) mark[h] <- mark[h] + 1/m
+    for (i in 1:row){
+      x1 <- xx[-i,]
+      class1 <- parsen(x1,xx[i,1:col],params[h],K)
+      class2 <- xx[i,col+1]
+      if (class1 != class2) mark[h] <- mark[h] + 1/row
     }
   }
 }
